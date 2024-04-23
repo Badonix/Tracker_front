c1df137b390887866fb9fff4053996d48ad6d4c0
Nick <nikdanelia@gmail.com>
2024-04-23 23:53:01 +0400

    there was ; floating around

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import Layout from "./layout";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </Layout>
   );
 }
